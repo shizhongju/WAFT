82fe43f687ae13ca57d25869600af3dc656d2dd2
shizhongju <100832731+shizhongju@users.noreply.github.com>
2024-03-09 20:50:29 +0800

    Update WAFT.m

--- WAFT.m
+++ WAFT.m
@@ -9,7 +9,13 @@ function WAFT_signal_out = WAFT(fft_length,alpha,beta,signal_in)
 % % Output:
 % % WAFT_signal_out  -Modulated signal and it's a matrix whose length is fft_length in column.
 %  wl(alpha) = cos((alpha-l)*pi/4).*cos(2*(alpha-l)*pi/4).*exp(3*(alpha-l)*pi*1i/4);which is the weight coefficient for the corresponding function
-
+%%      p_matrix : permutation matrix .F^2=p_matrix;
+        p_matrix = zeros(fft_length,fft_length);  
+        temp = [1 zeros(1,fft_length-1)];
+        p_matrix(1,:) = temp;
+        for i=1:fft_length-1
+            p_matrix(i+1,:) = circshift(temp,-i);
+        end
 w0 = cos((alpha-0)*pi/4).*cos(2*(alpha-0)*pi/4).*exp(3*(alpha-0)*pi*1i/4);
 w1 = cos((alpha-1)*pi/4).*cos(2*(alpha-1)*pi/4).*exp(3*(alpha-1)*pi*1i/4);
 w2 = cos((alpha-2)*pi/4).*cos(2*(alpha-2)*pi/4).*exp(3*(alpha-2)*pi*1i/4);
@@ -20,7 +26,8 @@ F = dftmtx(N)/sqrt(N);  % normalized discrete Fourier transform matrix"
 Lambda_mtx = zeros(fft_length,fft_length);
 i = 0:fft_length-1; 
 Lambda_mtx = diag(exp(-1j*2*pi*beta*i.^2));
-%% acquire WAFT signal
-WAFT_matrix = Lambda_mtx'*(w0*eye(fft_length)+w1*F+w2*F^2+w3*F^3)*Lambda_mtx
-WAFT_signal_out = WAFT_matrix*signal_in;
+%% acquire WAFT signal   WAFT_matrix = Lambda_mtx'*(w0*eye(fft_length) + w1*F + w2*p_matrix + w3*p_matrix*F)*Lambda_mtx;
+WAFT_signal_out = w0*signal_in+w1*Lambda_mtx'*fft(Lambda_mtx*signal_in,fft_length,1) + w2*Lambda_mtx'*p_matrix*Lambda_mtx*...
+signal_in + w3*Lambda_mtx'*p_matrix*fft(Lambda_mtx*signal_in,fft_length,1);
+
 end
